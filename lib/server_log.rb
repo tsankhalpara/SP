e4529cfaa32bb093d97eb74b1e801e09d9262091
Tara Sankhalpara <tsankhalpara@mac.com>
2019-07-03 13:42:15 +0100

    create server_log lib file to pass test

--- lib/server_log.rb
+++ lib/server_log.rb
@@ -1,1 +1,18 @@
+class ServerLog
 
+  attr_reader :server_list
+
+  def initialize
+    @server_list = {}
+  end
+
+  def create_hash(filename)
+    File.open(filename).each do |webserver_site|
+      site = webserver_site.split(' ')
+      if @server_list.has_key?(site[0])
+        @server_list[site[0]] << (site[1])
+      else @server_list[site[0]] = [site[1]]
+      end
+    end
+  end
+end
